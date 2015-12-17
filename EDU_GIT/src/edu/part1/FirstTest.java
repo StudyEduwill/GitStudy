4ded9f0be83af678f65a19b5895e24057e5bf179
eduwilldev <eduwilldev@gmail.com>
2015-12-17 17:41:00 +0900

    수정테스트2

--- EDU_GIT/src/edu/part1/FirstTest.java
+++ EDU_GIT/src/edu/part1/FirstTest.java
@@ -3,10 +3,13 @@ package edu.part1;
 public class FirstTest {
 	public static void main(String[] ar){
 		System.out.println("Hello World");
-		System.out.println("GoodBye Eduwill test2");
+		System.out.println("GoodBye Eduwill");
 		System.out.println("This is orange");
 		System.out.println("This is apple");
 		System.out.println("Hello git?");
 		System.out.println("why not?");
+		
+		
+		System.out.println("what?");
 	}
 }
