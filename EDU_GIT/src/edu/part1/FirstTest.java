ac8738bb71075d26722efe890d7946261e876147
chheo <chheo@eduwill.net>
2015-12-17 13:34:50 +0900

    *Orange

--- EDU_GIT/src/edu/part1/FirstTest.java
+++ EDU_GIT/src/edu/part1/FirstTest.java
@@ -4,5 +4,6 @@ public class FirstTest {
 	public static void main(String[] ar){
 		System.out.println("Hello World");
 		System.out.println("GoodBye Eduwill");
+		System.out.println("This is orange");
 	}
 }
