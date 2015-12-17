42de6d009671b78eb37613c2ebe6e4b328118751
kumjongwon <kumjongwon@daum.net>
2015-12-17 17:29:22 +0900

    test

--- EDU_GIT/src/edu/part1/FirstTest.java
+++ EDU_GIT/src/edu/part1/FirstTest.java
@@ -3,7 +3,7 @@ package edu.part1;
 public class FirstTest {
 	public static void main(String[] ar){
 		System.out.println("Hello World");
-		System.out.println("GoodBye Eduwill");
+		System.out.println("GoddodBye Eduwill");
 		System.out.println("This is orange");
 		System.out.println("This is apple");
 		System.out.println("Hello git?");
